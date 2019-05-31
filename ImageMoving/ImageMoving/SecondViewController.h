4d28230532fb2c328d764724491876e5d522ddf9
Anna <annaersh1994@gmail.com>
2019-05-31 21:53:58 +0100

    begin to work, added 2 screens

---
+++ ImageMoving/ImageMoving/SecondViewController.h
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.h
+//  ImageMoving
+//
+//  Created by Anna Ershova on 5/31/19.
+//  Copyright © 2019 Anna Ershova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+@interface SecondViewController : UIViewController
+
+@end
+
+
---
+++ ImageMoving/ImageMoving/SecondViewController.m
@@ -0,0 +1,32 @@
+//
+//  SecondViewController.m
+//  ImageMoving
+//
+//  Created by Anna Ershova on 5/31/19.
+//  Copyright © 2019 Anna Ershova. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
